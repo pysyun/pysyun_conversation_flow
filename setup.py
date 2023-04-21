b2a933f37fae49c25ac64f8f829dc008d1b124ef
pysyun <48061047+pysyun@users.noreply.github.com>
2023-04-22 01:32:39 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author='Py Syun',
     author_email='pysyun@vitche.com',
     py_modules=['pysyun.conversation.flow.console_bot', 'pysyun.conversation.flow.dialog_state_machine',
-                'pysyun.conversation.flow.redux', 'pysyun.conversation.flow.telegram_bot'],
+                'pysyun.conversation.flow.redux', 'pysyun.conversation.flow.telegram_bot',
+               'pysyun.conversation.flow.persistent_telegram_bot'],
     install_requires=['asyncio', 'Levenshtein', 'telegram', 'python-telegram-bot', 'graphviz']
 )

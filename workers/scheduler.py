c79ba7aa3f1949560133f29f39c65ccbe12b08e2
Py Syun <pysyun@vitche.com>
2024-07-23 03:44:26 +0300

    )

--- workers/scheduler.py
+++ workers/scheduler.py
@@ -1,11 +1,13 @@
+import asyncio
+
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
+from telegram import User
 
 
 class Scheduler:
 
     def __init__(self, injected_message, minute='*', hour='*', day='*', month='*', day_of_week='*'):
-
         self.state_machine = None
 
         self.scheduler = BackgroundScheduler()
@@ -23,8 +25,23 @@ class Scheduler:
         cron_trigger = CronTrigger(minute=self.minute, hour=self.hour, day=self.day, month=self.month,
                                    day_of_week=self.day_of_week)
 
-        self.scheduler.add_job(self.process, trigger=cron_trigger)
+        def step():
+            asyncio.run(self.process())
+
+        self.scheduler.add_job(step, trigger=cron_trigger)
         self.scheduler.start()
 
-    def process(self):
-        print(self.injected_message)
+    async def process(self):
+        await self.state_machine.process({
+            "update": {
+                "message": {
+                    "from_user": User(id=0, first_name='', is_bot=False)
+                },
+                "effective_chat": {
+                    "id": 0
+                }
+            },
+            # TODO: Pass the context
+            "context": None,
+            "text": self.injected_message
+        })

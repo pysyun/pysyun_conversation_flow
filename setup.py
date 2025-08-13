4b79066c7999a9c6e824775f7f1fc1305a166eeb
pysyun <48061047+pysyun@users.noreply.github.com>
2025-08-14 00:26:51 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     py_modules=['pysyun.conversation.flow.console_bot', 'pysyun.conversation.flow.dialog_state_machine',
                 'pysyun.conversation.flow.redux', 'pysyun.conversation.flow.telegram_bot',
                 'pysyun.conversation.flow.persistent_telegram_bot',
-                'pysyun.conversation.workers.abandoned_chat_scheduler'],
+                'pysyun.conversation.workers.abandoned_chat_scheduler', 
+                'pysyun.conversation.workers.state_machine_scheduler'],
     install_requires=['asyncio', 'Levenshtein', 'telegram', 'python-telegram-bot', 'graphviz', 'APScheduler']
 )
